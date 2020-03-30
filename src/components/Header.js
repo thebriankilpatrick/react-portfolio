667db9e43959c907b16e8c2bf52ee35fc3e7935e
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-03-29 23:18:35 -0400

    Added About page skeleton, and hid nav brand on med screen and down for proper styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <nav className="grey darken-4">
+        <nav className="transparent">
             <div className="nav-wrapper">
-            <Link to="/" className="brand-logo">Brian Kilpatrick</Link>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <Link to="/" className="brand-logo hide-on-med-and-down">Brian Kilpatrick</Link>
+            <ul id="nav-mobile" class="right">
                 <li><Link to="/">About</Link></li>
                 <li><Link to="/projects">Projects</Link></li>
                 <li><Link to="/resume">Resume</Link></li>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,14 @@ import "./HomePage.css";
 function HomePage() {
     return (
         <div>
-            
+            <div className="container center-align">
+                <div className="homePageTxt">
+                    <h1>Hi, I'm Brian Kilpatrick</h1>
+                    <h4>Full Stack Web Developer</h4>
+                    <h4>Richmond, VA</h4>
+                    <i class="fas fa-sort-down fa-4x"></i>
+                </div>
+            </div>
         </div>
     )
 }
